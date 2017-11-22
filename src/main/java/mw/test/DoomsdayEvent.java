1ee4d0effe56543f97128b2d46c619a2bbddf738
mwiesiolek <maksymilian.wiesiolek@gmail.com>
2017-11-22 12:52:29 +0100

    adds doomsday event!

---
+++ src/main/java/mw/test/DoomsdayEvent.java
@@ -0,0 +1,12 @@
+package mw.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+public class DoomsdayEvent {
+    private Date when;
+}
--- src/main/java/mw/test/ProcessShutdownListener.java
+++ src/main/java/mw/test/ProcessShutdownListener.java
@@ -1,20 +1,38 @@
 package mw.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
 public class ProcessShutdownListener implements ApplicationListener<ContextClosedEvent> {
 
+    private boolean dead;
+
     public ProcessShutdownListener() {
         log.info("App initialized.");
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(() -> {
+            if (!dead) {
+                log.info("Processing...");
+            }
+        }, 0, 2, TimeUnit.SECONDS);
     }
 
     @Override
     public void onApplicationEvent(ContextClosedEvent event) {
+        dead = true;
+
         try {
             destroy();
         } catch (InterruptedException e) {
